0f51848591cb1ce80f876b56be2fe87556d8356d
Ankit Gupta <ankitgupta93727@gmail.com>
2026-01-01 09:29:09 +0530

    controlling movement

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ board.style.gridTemplateColumns=`repeat(${col},${blocksize}px)`;
 board.style.gridTemplateRows=`repeat(${row},${blocksize}px)`;
 let blocks=[];
 const snake=[
-    {x:3,y:3},
-    {x:4,y:3},
-    {x:5,y:3}
+    {x:5,y:3},
+    // {x:4,y:3},
+    // {x:3,y:3}
 ];
 let direction="right";
 for(let r=0;r<row;r++){
@@ -17,29 +17,49 @@ for(let r=0;r<row;r++){
         let block=document.createElement("div");
         block.classList.add("innerBlock");
         board.appendChild(block);
-        block.innerText=`${r},${c}`;
+        // block.innerText=`${r},${c}`;
         blocks[`${r}-${c}`]=block;
     }
 }
 function renderSnake(){
     for(let i=0;i<snake.length;i++){
-        let block=blocks[`${snake[i].y}-${snake[i].x}`];
+        let block=blocks[`${snake[i].x}-${snake[i].y}`];
         block.classList.add("fill");
     }}
     setInterval(()=>{
         let head=null;
+        let snakeSize=snake.length-1;
+        let tailBlock=[`${snake[snakeSize].x}-${snake[snakeSize].y}`];
+        blocks[tailBlock].classList.remove("fill");
         if(direction=="left"){
-            head={x:snake[0].x-1,y:snake[0].y};
-        }
-        if(direction=="right"){
-            head={x:snake[0].x+1,y:snake[0].y};
-        }
-        if(direction=="top"){
             head={x:snake[0].x,y:snake[0].y-1};
         }
-        if(direction=="down"){
+        if(direction=="right"){
             head={x:snake[0].x,y:snake[0].y+1};
         }
+        if(direction=="top"){
+            head={x:snake[0].x-1,y:snake[0].y};
+        }
+        if(direction=="down"){
+            head={x:snake[0].x+1,y:snake[0].y};
+        }
         snake.unshift(head);
+        snake.pop();
         renderSnake();
     },500);
+    addEventListener("keydown",(e)=>{
+        console.log(e.key);
+        if(e.key=="ArrowLeft"){
+            direction="left";
+        }
+        if(e.key=="ArrowRight"){
+            direction="right";
+        }
+        if(e.key=="ArrowUp"){
+            direction="top";
+        }
+        if(e.key=="ArrowDown"){
+            direction="down";
+        }
+        
+    })
